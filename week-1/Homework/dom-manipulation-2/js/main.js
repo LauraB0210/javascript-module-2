531f1da6c19c09d6463947db76582b103b8c2c62
Laura Beleño <vaneessa10@gmail.com>
2022-10-01 12:41:12 +0200

    ejercicios finalizados semana 1

--- week-1/Homework/dom-manipulation-2/js/main.js
+++ week-1/Homework/dom-manipulation-2/js/main.js
@@ -1,76 +1,111 @@
+
 const myPage = document.querySelector("#blueBtn");
-myPage.addEventListener("click", cambioColor);
-
-function cambioColor (){
-let myElement = document.querySelector(".btn-lrg");
+myPage.addEventListener("click",()=> {
+    let myElement = document.querySelector(".btn-lrg");
 myElement.style.backgroundColor = "#ffa500";
-}
 
-const myPage1 = document.querySelector("#blueBtn");
-myPage1.addEventListener("click", cambioColor1);
-
-function cambioColor1 (){
 let myElement1 = document.querySelector(".jumbotron");
 myElement1.style.backgroundColor = "#588fbd";
-}
 
-const myPage2 = document.querySelector("#blueBtn");
-myPage2.addEventListener("click", cambioColor2);
-
-function cambioColor2 (){
-    let myElement2  = document.querySelector(".btn-secondary");
-    myElement2.style.backgroundColor = "black"
-    myElement2.style.color = "white"
-}
+let myElement2  = document.querySelector(".btn-secondary");
+     myElement2.style.backgroundColor = "black"
+     myElement2.style.color = "white"
+});
 
 const myOrange = document.querySelector("#orangeBtn");
-myOrange.addEventListener("click", cambioColorO);
+myOrange.addEventListener("click",() => {
 
-function cambioColorO (){
     let myOrange = document.querySelector(".jumbotron");
-    myOrange.style.backgroundColor = "#f0ad4e"
-}
+     myOrange.style.backgroundColor = "#f0ad4e"
 
-const myOrange1 = document.querySelector("#orangeBtn");
-myOrange1.addEventListener("click", cambioColorO1);
+     let myOrange1 = document.querySelector(".btn-lrg");
+     myOrange1.style.backgroundColor = "#5751fd"
 
-function cambioColorO1 (){
-    let myOrange1 = document.querySelector(".btn-lrg");
-    myOrange1.style.backgroundColor = "#5751fd"
-}
+      let myOrange2 = document.querySelector(".btn-secondary");
+     myOrange2.style.backgroundColor = "#31b0d5"
+     myOrange2.style.color = "white"
 
-const myOrange2 = document.querySelector("#orangeBtn");
-myOrange2.addEventListener("click", cambioColorO2);
+});
 
-function cambioColorO2 (){
-    let myOrange2 = document.querySelector(".btn-secondary");
-    myOrange2.style.backgroundColor = "#31b0d5"
-    myOrange2.style.color = "white"
-}
 
-const myGreen = document.querySelector("#greenBtn");
-myGreen.addEventListener("click", cambioColorG);
+ const myGreen = document.querySelector("#greenBtn");
+ myGreen.addEventListener("click",() => {
 
-function cambioColorG(){
     let myGreen = document.querySelector(".jumbotron");
-    myGreen.style.backgroundColor = "#87ca8a"
+     myGreen.style.backgroundColor = "#87ca8a"
+
+     let myGreen1 = document.querySelector(".btn-lrg");
+     myGreen1.style.backgroundColor = "black"
+
+     let myGreen2 = document.querySelector(".btn-secondary");
+     myGreen2.style.backgroundColor = "#8c9c08"
+
+ });
+
+
+//parte2//
+
+const botonsubmit = document.querySelector("#btn-submit");
+botonsubmit.addEventListener("click", verificacion);
+
+function verificacion (event){
+    event.preventDefault();
+   let exito = 1;
+
+    //paso 1 : validar el campo de correo//
+
+
+ function validar_email( email ) 
+ {
+     const regex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+     return regex.test(email) ? true : false;
+ }
+
+  if( validar_email( ) )
+  {
+      alert("El email es correcto");
+  }
+  else
+  {
+      alert("El email NO es correcto");
+     
+  }
+
+ 
+
+    //paso 2: validar el campo de nombre//
+    const name = document.querySelector("#example-text-input");
+    const namelength = name.value.length;
+    if (namelength == 0) {
+        exito = 0;
+        name.style.backgroundColor = "red";
+  
+    }
+
+
+    //paso 3: validar el campo de descripcion//
+
+     const namedesc = document.querySelector("#exampleTextarea");
+    const namelengthdesc = namedesc.value.length;
+    if (namelengthdesc == 0) {
+        exito = 0;
+         namedesc.style.backgroundColor = "red";
+  
+    }
+
+    //paso 4: si todo va bien, mostrar una alerta y vaciar el formulario//
+   if (exito ==1) {
+        alert("Thanks for visiting Bikes for Refugees!");
+
+        const formulary = document.querySelector("#form-registro");
+         formulary.reset();
+
+   } 
+   
+
 }
 
-const myGreen1 = document.querySelector("#greenBtn");
-myGreen1.addEventListener("click", cambioColorG1);
 
-function cambioColorG1(){
-    let myGreen1 = document.querySelector(".btn-lrg");
-    myGreen1.style.backgroundColor = "black"
-}
-
-const myGreen2 = document.querySelector("#greenBtn");
-myGreen2.addEventListener("click", cambioColorG2);
-
-function cambioColorG2(){
-    let myGreen2 = document.querySelector(".btn-secondary");
-    myGreen2.style.backgroundColor = "#8c9c08"
-}
 
 
 
