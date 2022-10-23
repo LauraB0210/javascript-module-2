d695a7dc0d9015f68d7d69002684dbc31dcc0c95
Laura Beleño <vaneessa10@gmail.com>
2022-10-23 15:01:05 +0200

    ejercicio A week 3 JS2

--- week-3/Homework/A-timeout/exercise.js
+++ week-3/Homework/A-timeout/exercise.js
@@ -5,7 +5,20 @@ EXERCISE 1
 Task 1
 Using setTimeout, change the background colour of the page after 5 seconds (5000 milliseconds). Hint: try searching for setTimeout. (https://www.w3schools.com/jsref/met_win_settimeout.asp)
 
+
+
+
 Task 2
 Update your code to make the colour change every 5 seconds to something different. Hint: try searching for setInterval. (https://www.w3schools.com/jsref/met_win_setinterval.asp)
 ================
 */
+
+const myTimeout = setInterval(myGreeting, 5000)
+
+function myGreeting (){
+    const colorBody = document.querySelector("body")
+    colorBody.style.backgroundColor = colorBody.style.backgroundColor == "blue" ? "pink" : "blue"
+}
+function  pausaColor(){
+    clearInterval(myTimeout);
+}
